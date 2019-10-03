4905a38c976c5f0e86b8f6c212b491d34406ab88
evapapazian <eva.papazian@ieseg.fr>
2019-10-03 10:25:02 +0200

    projet 0

---
+++ 00_hello.rb
@@ -0,0 +1,21 @@
+def ask_first_name
+	puts "Comment tu t'appelles ?"
+	print ">"
+	first_name = gets.chomp
+return first_name
+end 
+
+
+
+
+def say_hello(first_name)
+ 	
+puts "Bonjour #{first_name}!"
+end
+
+def perform 
+	first_name = ask_first_name
+	say_hello(first_name)
+end
+
+perform 
